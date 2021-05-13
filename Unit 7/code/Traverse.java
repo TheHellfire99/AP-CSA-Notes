7d0dea0b0212aeab51fe1d9f08bd3a844ba627dd
Shaurya Srivastav <shau@Shauryas-MacBook-Air.local>
2021-05-13 00:37:25 -0700

    unit 7 but more stuff needs to be added

---
+++ Unit 7/code/Traverse.java
@@ -0,0 +1,21 @@
+//purpose : to traverse through an arrayList and 
+//print sum of all characters present in the list
+
+public class Traverse {
+    public static void main(String[] args){
+        int sum = 0; 
+        for(int i = 0; i <= roster.size() - 1; i++){
+            sum += roster.get(i).length(); 
+        }
+        System.out.println(sum); 
+
+        //while loop method 
+        int sum = 0; 
+        int i=0; 
+        while (i < arr.size()){
+            sum += roster.get(i).lenght(); 
+            i++; 
+        }
+        System.out.println(sum);  
+    }
+}
